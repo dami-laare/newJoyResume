6cd96def7aac73e37a920774216cc143193116f3
Temi Osunkiyesi <osunkiyesitayo@gmail.com>
2021-10-11 16:28:32 +0100

    Added allowed host

--- joyresumesite/settings.py
+++ joyresumesite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g5p16)+9z+bux!owp+5gc^_r_gji8t)#l$9reu(v-c50=y^&jh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['joyaifuobhokhan.pythonanywhere.com']
 
 
 # Application definition
